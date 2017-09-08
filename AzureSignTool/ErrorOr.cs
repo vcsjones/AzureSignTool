8ea4935cf66fba4f7575565674fbcd9859dfe949
Kevin Jones <kevin.jones@ankuraconsulting.com>
2017-09-08 17:58:22 -0400

    Additional certs for chain building.
    This allows specifying additional certificates for path building.

---
+++ AzureSignTool/ErrorOr.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace AzureSignTool
+{
+    public class ErrorOr<TValue>
+    {
+        private readonly TValue _value;
+        private readonly Exception _error;
+
+        private ErrorOr(TValue value) => _value = value;
+
+        private ErrorOr(Exception ex) => _error = ex;
+
+        public static implicit operator ErrorOr<TValue>(TValue value) => new ErrorOr<TValue>(value);
+        public static implicit operator ErrorOr<TValue>(Exception ex) => new ErrorOr<TValue>(ex);
+
+        public class Ok : ErrorOr<TValue>
+        {
+            private Ok(TValue value) : base(value)
+            {
+            }
+
+            public TValue Value => _value;
+        }
+
+        public class Err : ErrorOr<TValue>
+        {
+            private Err(Exception ex) : base(ex)
+            {
+            }
+
+            public Exception Error => _error;
+        }
+    }
+}
