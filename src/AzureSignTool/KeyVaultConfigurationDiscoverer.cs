ca091cecf201cf07dde873aa39b5b4130e5c61f4
Sean Killeen <SeanKilleen@gmail.com>
2020-05-16 14:47:09 -0400

    Add additional logging when KeyVaultError occurs

--- src/AzureSignTool/KeyVaultConfigurationDiscoverer.cs
+++ src/AzureSignTool/KeyVaultConfigurationDiscoverer.cs
@@ -47,6 +47,25 @@ namespace AzureSignTool
                 
                 certificate = new X509Certificate2(azureCertificate.Cer);
             }
+            catch (KeyVaultErrorException kvException)
+            {
+                if (authenticationFailure)
+                {
+                    return kvException;
+                }
+
+                _logger.LogError($"Failed to retrieve certificate {configuration.AzureKeyVaultCertificateName} from Azure Key Vault. Please verify the name of the certificate and the permissions to the certificate.");
+
+                var error = kvException.Body.Error;
+                _logger.LogError($"KeyVault Error Code: {error.Code}, Message: {error.Message}");
+
+                if (error.InnerError != null)
+                {
+                    _logger.LogError($"KeyVault Inner Error Code: {error.InnerError.Code}, Message: {error.InnerError.Message}");
+                }
+
+                return kvException;
+            }
             catch (Exception e)
             {
                  _logger.LogError($"Failed to retrieve certificate {configuration.AzureKeyVaultCertificateName} from Azure Key Vault. Please verify the name of the certificate and the permissions to the certificate.");
