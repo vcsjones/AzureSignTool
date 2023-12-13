2cf2d17736eef7a81cb73147e394c6737c5dc7c5
Kevin Jones <vcsjones@github.com>
2023-12-13 11:23:07 -0500

    Apply suggestions from code review

--- src/AzureSign.Core/AuthenticodeKeyVaultSigner.cs
+++ src/AzureSign.Core/AuthenticodeKeyVaultSigner.cs
@@ -74,9 +74,10 @@ namespace AzureSign.Core
         /// <param name="description">The description to apply to the signature.</param>
         /// <param name="path">The path to the file to signed.</param>
         /// <param name="logger">An optional logger to capture signing operations.</param>
-        /// <param name="appendSignature">Instruct if should append the new signature to existing, if false - remove existing signatures.</param>
+        /// <param name="appendSignature"><see langword="true"/> if the signature should be appended to an existing signature. When <see langword="false"/>, any existing signatures will be replaced.</param>
         /// <returns>A HRESULT indicating the result of the signing operation. S_OK, or zero, is returned if the signing
         /// operation completed successfully.</returns>
+        <exception cref="PlatformNotSupportedException"><paramref name="appendSignature"/> was set to <see langword="true"/> however the current operating system does not support appending signatures.</exception>
         public unsafe int SignFile(ReadOnlySpan<char> path, ReadOnlySpan<char> description, ReadOnlySpan<char> descriptionUrl, bool? pageHashing, ILogger? logger = null, bool appendSignature = false)
         {
             static char[] NullTerminate(ReadOnlySpan<char> str)
