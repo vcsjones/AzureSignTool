069c403167f9d251c0dd77d4b8be4ed2d2d51e6a
Roel van der Steen <roel@tacx.com>
2024-02-11 12:17:21 +0100

    Fix validation error in SignCommand.cs
    The commit fixes a validation error in the SignCommand.cs file. The code now correctly checks for the use of '--quiet' and '--verbose' options together, returning an appropriate validation result.

--- src/AzureSignTool/SignCommand.cs
+++ src/AzureSignTool/SignCommand.cs
@@ -148,7 +148,7 @@ namespace AzureSignTool
             }
             if (Quiet && Verbose)
             {
-                return new ValidationResult("Cannot use '--quiet' and '--verbose' options together.", new[] { nameof(NoPageHashing), nameof(PageHashing) });
+                return new ValidationResult("Cannot use '--quiet' and '--verbose' options together.", new[] { nameof(Quiet), nameof(Verbose) });
             }
             if (!OneTrue(KeyVaultAccessToken.Present, KeyVaultClientId.Present, UseManagedIdentity))
             {
